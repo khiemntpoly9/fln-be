959f7c8088c87214d40069acbd72eae363e0fe19
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-08-06 20:17:48 +0700

    up

--- src/modules/product/dto/images.dto.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { ApiProperty } from '@nestjs/swagger';
-export class imagesDto {
-	@ApiProperty()
-	public_id: string;
-
-	@ApiProperty()
-	secure_url: string;
-}
