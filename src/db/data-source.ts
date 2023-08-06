52283569a3c9460e10272956b68fcdae2cb7cc33
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-08-06 20:14:33 +0700

    up

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -4,19 +4,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  charset: 'utf8mb4_unicode_ci',
-  timezone: '+07:00',
-  logging: false,
-  synchronize: false,
-  subscribers: [],
-  migrations: [],
+	type: 'mysql',
+	host: process.env.DB_HOST,
+	port: 3306,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	entities: [__dirname + '/**/*.entity{.ts,.js}'],
+	charset: 'utf8mb4_unicode_ci',
+	timezone: '+07:00',
+	logging: false,
+	synchronize: false,
+	subscribers: [],
+	migrations: [],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
