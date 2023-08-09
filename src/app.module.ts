47ac4439b6a8d689b12a68227ca8a33db1ca4106
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-08-09 15:51:15 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 /* */
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './modules/product/product.module';
 import { dataSourceOptions } from './db/data-source';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -15,9 +14,6 @@ import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { MailModule } from './modules/mail/mail.module';
 import { PassportModule } from '@nestjs/passport';
-import { FavoriteModule } from './modules/favorite/favorite.module';
-import { BrandModule } from './modules/brand/brand.module';
-import { OrderModule } from './modules/order/order.module';
 import { ManagerModule } from './modules/manager/manager.module';
 /* */
 
@@ -25,16 +21,12 @@ import { ManagerModule } from './modules/manager/manager.module';
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		TypeOrmModule.forRoot(dataSourceOptions),
-		ProductModule,
 		UserModule,
 		AuthModule,
 		CategoriesModule,
 		CloudinaryModule,
 		MailModule,
 		PassportModule,
-		FavoriteModule,
-		BrandModule,
-		OrderModule,
 		ManagerModule,
 	],
 	controllers: [AppController],
--- src/modules/favorite/dto/create-favorite.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateFavoriteDto {
-	@ApiProperty()
-	id_product: number;
-}
